2d7109338e8f8f97c7a030a2f37c9cf595e3a726
Daniel Ortega <49536646+danieldortega77@users.noreply.github.com>
2021-10-13 00:48:59 -0400

    Experiment with shaders
    Playing around with OSL in order to texture solid objects with Perlin-noise based procedural textures

--- genTexture.py
+++ genTexture.py
@@ -1,5 +1,8 @@
 import bpy
 noise = bpy.data.texts['noise.py'].as_module()
 
-
 pnf = noise.PerlinNoiseFactory(2, tile=(0, 3))
+
+cube = bpy.ops.mesh.primitive_cube_add(enter_editmode=False, align='WORLD', location=(0, 0, 0), scale=(1, 1, 1))
+newMat = bpy.ops.material.new()
+#noiseNode = bpy.ops.node.add_node(type="ShaderNodeTexNoise", use_transform=True)
